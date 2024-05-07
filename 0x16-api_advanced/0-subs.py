58176c7c61a581c798b2c41780d4f34d15f60ff3
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-05-07 13:41:18 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import requests
+
 def number_of_subscribers(subereddit):
     """A function to get the total number of
         of subscribers of a subreddit API
@@ -10,6 +12,6 @@ def number_of_subscribers(subereddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    headers = {"User-Agent": "Google Chrome Version 81.0.4044.129"}
+    headers = {"User-Agent": "Google Chrome Version 124.0.6367.119"}
     req = requests.get(f'https://www.reddit.com/r/{subreddit}/about.json',headers=headers)
     return (req.json().get('data').get('subscribers'))
